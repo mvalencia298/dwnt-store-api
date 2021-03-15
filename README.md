24d250156209f1729c9a0d23cff15cc9d6af7e4d
mvalencia298 <mauro.valc@gmail.com>
2021-03-15 03:22:02 -0500

    add default files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Integrantes
+- Mauricio Valencia Cosme
--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 // create express app
 const app = express();
 // Setup server port
 const port = process.env.PORT || 5000;
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
-// parse requests of content-type - application/json
-app.use(bodyParser.json())
 // define a root route
 app.get('/', (req, res) => {
   res.send("Hello World");
